a75de7fff80342787e16b3a026650db696b69c37
M0urban <114688603+M0urban@users.noreply.github.com>
2024-03-30 11:27:39 +0100

    changed neo tree settings to show fullscreen on startup

--- lua/config/neo-tree-config.lua
+++ lua/config/neo-tree-config.lua
@@ -1,10 +1,17 @@
---[[ require("neo-tree").setup({
+require("neo-tree").setup({
   window = {
     mappings = {
       ["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
     }
   },
-}) ]]
+  filesystem = {
+    hijack_netrw_behavior = "open_current", --set "disabled"
+  },
+  source_selector = {
+    content_layout = "center",
+    tabs_layout = "center",
+  },
+})
 
 vim.keymap.set('n', '<leader>ft',
   function()
