5d79f3fe9611417f9a14a46bd7370f9e01025edd
M0urban <114688603+M0urban@users.noreply.github.com>
2024-05-19 23:25:09 +0200

    Changed keymaps for new capabilities of nvim 0.10

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,10 +7,10 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+vim.keymap.set('n', '<leader>h', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end)
 
 -- Usefull remaps
 vim.keymap.set('n', '<leader>rw', vim.cmd.Ex)
