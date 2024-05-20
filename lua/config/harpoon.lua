8095349fa70e4dcfbd5695458103198173810efd
M0urban <114688603+M0urban@users.noreply.github.com>
2024-05-20 17:15:22 +0200

    Changed harpoon shorcuts to not conflict

--- lua/config/harpoon.lua
+++ lua/config/harpoon.lua
@@ -7,10 +7,10 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>o", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-w>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-r>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-u>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-o>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-z>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-b>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<leader>p", function() harpoon:list():prev() end)
