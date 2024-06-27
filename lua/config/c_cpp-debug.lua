8b159794106bb61d38fc965c998f393da1db0e8a
Marius Urban <marius.urban@stackforce.de>
2024-06-27 13:43:32 +0200

    updates and misc stuff

--- lua/config/c_cpp-debug.lua
+++ lua/config/c_cpp-debug.lua
@@ -11,6 +11,17 @@ dap.configurations.cpp = {
     stopAtEntry = true,
   },
   {
+    name = "Launch file with args",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    args = {"-i"},
+    cwd = '${workspaceFolder}',
+    stopAtEntry = true,
+  },
+  {
     name = 'Attach to gdbserver :1234',
     type = 'cppdbg',
     request = 'launch',
--- lua/config/cmake-tools-config.lua
+++ lua/config/cmake-tools-config.lua
@@ -10,6 +10,13 @@ function M.cmake_config()
     --       ${variant:xx}
     cmake_build_directory = "build",-- "build/${variant:buildType}", -- this is used to specify generate directory for cmake, allows macro expansion, relative to vim.loop.cwd()
     cmake_soft_link_compile_commands = true,          -- this will automatically make a soft link from compile commands file to project root dir
+    cmake_executor = {
+      default_opts = {
+        quickfix = {
+          auto_close_when_success = true,
+        },
+      },
+    },
   }
 end
 
