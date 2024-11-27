7ab190935e39006d15f7ea5a3d76145bc4a250c7
Marius Urban <marius.urban@stackforce.de>
2024-11-27 09:24:38 +0100

    commented out alternative setup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -29,10 +29,10 @@ require('lazy').setup({
       enabled = function(root_dir)
         return vim.g.lazydev_enabled == nil and true or vim.g.lazydev_enabled
       end,
-      -- disable when a .luarc.json file is found
-      enabled = function(root_dir)
-        return not vim.uv.fs_stat(root_dir .. "/.luarc.json")
-      end,
+      -- -- disable when a .luarc.json file is found
+      -- enabled = function(root_dir)
+      --   return not vim.uv.fs_stat(root_dir .. "/.luarc.json")
+      -- end,
     },
   },
   { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
