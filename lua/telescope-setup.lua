a41958fbb69937ff3dd19bd1d24c3730255b3cf9
Marius Urban <marius.urban@stackforce.de>
2024-11-29 15:31:27 +0100

    Made Telescope file finder follow symlinks

--- lua/telescope-setup.lua
+++ lua/telescope-setup.lua
@@ -76,9 +76,12 @@ vim.keymap.set('n', '<leader>/', function()
 end, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', function()
+    require('telescope.builtin').find_files({ follow = true, })
+  end,
+  { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>saf',
-  function() require('telescope.builtin').find_files({ hidden = true, no_ignore = true, }) end,
+  function() require('telescope.builtin').find_files({ follow = true, hidden = true, no_ignore = true, }) end,
   { desc = '[S]earch [A]ll [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
