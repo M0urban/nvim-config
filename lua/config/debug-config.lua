be1e0e7d0d73f2631ccbfc944f41c80bdd346b57
Marius Urban <114688603+M0urban@users.noreply.github.com>
2025-05-03 18:09:07 +0200

    Updated nvim dap continue script
    launch.json no longer needs to be loaded manually

--- lua/config/debug-config.lua
+++ lua/config/debug-config.lua
@@ -32,14 +32,7 @@ function M.dap_config()
   }
 
   --basic keymaps
-  vim.keymap.set('n', '<F5>', function()
-      -- (Re-)reads launch.json if present
-      if vim.fn.filereadable("./.vscode/launch.json") then
-        require("dap.ext.vscode").load_launchjs(nil, { cppdbg = { "c", "cpp" } })
-      end
-      require("dap").continue()
-    end,
-    { desc = 'Debug: Start/Continue' })
+  vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
   vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
   vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
   vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
