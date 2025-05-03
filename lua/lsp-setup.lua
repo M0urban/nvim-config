131e702b141712f67055e5e4890628c650848b3c
Marius Urban <114688603+M0urban@users.noreply.github.com>
2025-05-03 18:20:30 +0200

    Added some new diagnostic features

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -154,3 +154,14 @@ require('lspconfig').zls.setup {
   capabilities = capabilities,
 }
 
+-- enable autocompletion
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
+
+vim.diagnostic.config({ virtual_lines = { current_line = true }, })
