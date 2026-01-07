ba0c0451010c7aa9f122a70aff9a004b1a897cd8
Marius Urban <marius.urban@stackforce.de>
2026-01-07 09:39:21 +0100

    Added -j flag for cmake-tools
    This is due to cmake-tools no longer supporting evaluating -j$(nproc),
    so it has to be fetched before.

--- lua/config/cmake-tools-config.lua
+++ lua/config/cmake-tools-config.lua
@@ -1,9 +1,21 @@
 local M = {}
 
 function M.cmake_config()
+  local procs = vim.loop.cpu_info()
+  -- If number of processes cannot be found use 8 as default.
+  local num_of_procs = 8
+  if not (procs == nil) then
+    local count = 0
+    for _ in pairs(procs) do count = count + 1 end
+    if count > 0 then
+      num_of_procs = count
+    end
+  end
+
+  local num_of_procs_option = string.format("-j%d", num_of_procs)
   require('cmake-tools').setup {
     cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" }, -- this will be passed when invoke `CMakeGenerate`
-    cmake_build_options = {"-j$(nproc)"},                                         -- this will be passed when invoke `CMakeBuild`
+    cmake_build_options = {num_of_procs_option},                                         -- this will be passed when invoke `CMakeBuild`
     -- support macro expansion:
     --       ${kit}
     --       ${kitGenerator}
